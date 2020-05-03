d8c99ff1ee5d5d1e3399de93ff4bdc1008417653
Art Navsegda <artnavsegda@gmail.com>
2020-05-03 05:06:06 +0300

    Update MainActivity.java

--- hello/app/src/main/java/com/example/hello/MainActivity.java
+++ hello/app/src/main/java/com/example/hello/MainActivity.java
@@ -11,4 +11,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+    }
+
 }
